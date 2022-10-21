75da7d8227dd10c29462e889ac5054cd61c04c18
francezt <jeskon88@gmail.com>
2022-10-21 22:00:59 +0700

    menambahkan teks pada file ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class ForLoop{
 	public static void main(String[] args) {
-		
-		for(int i=0; i<5; i++){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Masukan nilai i > ");
+		int usr = in.nextInt();
+		for(int i=0; i<usr; i++){
 			System.out.print( (i+1) +" ");
 		}
 		System.out.println();
